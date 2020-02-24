fe871234262242ef29a281ea432290fa5ba6f9af
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2020-02-24 15:49:27 +0000

    Visual improvements on Search screen

--- screens/Search.js
+++ screens/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { View, Button } from 'react-native'
+import { View, Button, StyleSheet, Text } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
+
 
 export default class Search extends Component {
   constructor(props) {
@@ -12,10 +13,36 @@ export default class Search extends Component {
     const { navigation: { navigate } } = this.props
 
     return (
-      <View>
-        <TextInput onChangeText={(text) => this.setState({text})} />
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({text})}
+            autoFocus={true}
+            returnKeyType='search'
+            onEndEditing={(_event) => navigate('Results', { searchString: this.state.text })}
+          />
+          <Text>Search by album title or artist</Text>
+        </View>
         <Button title='Search' onPress={() => navigate('Results', { searchString: this.state.text })} />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  content: {
+    padding: 20,
+  },
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+    padding: 10,
+    fontSize: 30    
+  }
+})
